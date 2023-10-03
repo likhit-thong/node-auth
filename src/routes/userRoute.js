b7f2137706e10839d2f30a9e32db5f937e966e6b
likhit <mrlikhit1992@gmail.com>
2023-10-03 17:07:25 +0700

    second commit

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,13 +2,7 @@ const router = require("express").Router()
 
 router.route('/users')
   .get((req, res) => {
-    // res.setHeader( "Strict-Transport-Security", "max-age=5184000; includeSubDomains; preload" )
-    //res.header("Strict-Transport-Security", "max-age=5184000; includeSubDomains; preload")
     res.json({message: "GET all user"})
-    // res
-    //   .setHeader( "Strict-Transport-Security", "max-age=5184000; includeSubDomains; preload")
-    //   .json({message: "GET all user"})
-
   })
   .post((req, res) => {
     res.json({message: "POST created user!!"})
